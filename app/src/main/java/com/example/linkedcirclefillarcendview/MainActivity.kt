c6afa097851c1f67096f8c6a9766dc7dff6d5a70
Anwesh43 <anweshthecool0@gmail.com>
2021-01-06 02:11:11 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedcirclefillarcendview/MainActivity.kt
+++ app/src/main/java/com/example/linkedcirclefillarcendview/MainActivity.kt
@@ -2,9 +2,18 @@ package com.example.linkedcirclefillarcendview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.circlefillarcendview.CircleFillArcEndView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        CircleFillArcEndView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
